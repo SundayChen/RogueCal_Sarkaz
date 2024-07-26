87d870de0e9cdf67590a14d72c56c29b47dceff7
SundayChen <824987073@qq.com>
2024-07-27 01:18:02 +0800

    fix: add redirection when clicking source code

--- src/views/FooterView.vue
+++ src/views/FooterView.vue
@@ -1,7 +1,9 @@
 <template>
     <footer>
         <p>© 2024 可汗杯 萨卡兹肉鸽赛季</p>
-        <p>本站由 <a href="https://cn.vuejs.org/">Vue</a> 提供技术支持，项目源码地址 <a href="">GitHub</a></p>
+        <p>本站由 <a href="https://cn.vuejs.org/">Vue</a> 提供技术支持，项目源码地址
+            <a href="https://github.com/SundayChen/RogueCal_Sarkaz">GitHub</a>
+        </p>
     </footer>
 </template>
 
